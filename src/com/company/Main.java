59a05bde1dd925f78126c5bfdf408978bae534d6
silver-Sanchez <55756108+silver-Sanchez@users.noreply.github.com>
2020-04-06 11:33:50 +0300

    ввел свои

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("hello");
-        System.out.println("!");
+      // System.out.println("hello");
+       // System.out.println("!");
+        System.out.println("Hello, world!");
     }
 }
